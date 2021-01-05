b279f881d46e58e2396ba4e50f8f9d617ac3d01b
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-05 18:38:07 -0300

    fixed webpack style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -28,7 +29,15 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: 'css-loader'
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
       },
     ]
   },
@@ -37,6 +46,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       file: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 }
