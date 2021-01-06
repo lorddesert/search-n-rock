828197a30c1bde1bfa49ba9dc78ee63b4ca1a85b
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-06 15:57:16 -0300

    Loader component created

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="Loader">
+      <div className="vinil">
+        <span className="center">
+          Searching...
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Loader;
