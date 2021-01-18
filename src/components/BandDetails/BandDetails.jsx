36f59860313eec65b446d35e6e513f644027e711
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-18 01:11:34 -0300

    final styles addded, syntax changed and fixed.

--- src/components/BandDetails/BandDetails.jsx
+++ src/components/BandDetails/BandDetails.jsx
@@ -1,25 +1,64 @@
 import React from 'react'
 import './BandDetails.css';
+import membersSVG from './resources/membersSVG.svg';
+import moreOptions from './resources/moreOptions.svg';
 
 // Components
 import Band from '../Band/Band';
+import Album from '../Album/Album';
+import Member from '../Member/Member';
+
+// Scripts
+import { deleteBand, editBand } from '../scripts/crud';
+
+const handleEvent = () => {
+  const displayOptions = document.getElementById('displayOptions');
+
+  displayOptions.classList.toggle('display');
+}
 
 const BandDetails = props => {
   return (
-    <div className="BandDetails">
+    <div className='BandDetails'>
       <header>
-        <Band name={props.name} year={props.year} country={props.country} />
+        <Band band={props.selectedBand} inBandDetails={true}/>
+        <div>
+          <img className='options' src={moreOptions} alt="options" onClick={handleEvent}/>
+          <ul className='displayOptions' id='displayOptions'>
+            <li onClick={() => props.setShowEditBand(true)} >Edit</li>
+            <li onClick={() => deleteBand(props.selectedBand.id)} ><strong>Delete</strong></li>
+          </ul>
+        </div>
       </header>
-      <main>
-        <div className="Band-albums">
-        <h2>Albums</h2>
-          {props.albums}  
-        </div>  
-      </main>
-      <footer>
-        <div className="Band-members">
+      {props.selectedBand.albums &&
+        <main className='Band-albums-container'>
+            <h2>Albums</h2>
+          <div className='Band-albums'>
+            { props.selectedBand.albums[1] ? 
+              props.selectedBand.albums.map((album, i) => {
+                return(
+                  <Album
+                    key={i}
+                    album={album}
+                  />
+                );
+              })
+            :
+              <h2 className='not-finded'>We didn't find any</h2>
+            }  
+          </div>  
+        </main>
+      }
+      <footer className='BandDetails-footer'>
           <h2>Members</h2>
-          {props.members}
+          <img src={membersSVG} alt='svg'/>
+        <div className='Members'>
+          {props.selectedBand.members.map((member, i) => 
+              <Member
+                member={member.name}
+                key={i}
+              />)
+          }
         </div>
       </footer>
     </div> 
