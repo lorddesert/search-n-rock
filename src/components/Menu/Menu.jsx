580b5097912304ba0282f246a9ed643ba327f2b6
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-18 01:14:45 -0300

    syntax changed and final styles added.

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Menu.css';
 import menuImg from './resources/menu.svg';
 
-const Menu = () => {
+const handleEvent = () => {
+  const menuOptions = document.getElementById('menuOptions');
+
+  menuOptions.classList.toggle('display-Menu-options');
+
+  // } else {
+  //   menuOptions.style.display = 'block';
+
+  //   setTimeout(() => {
+  //     menuOptions.classList.toggle('display-Menu-options');
+  //   }, 50);
+}
+
+const Menu = props => {
   return (
-    <div className='Menu'>
-      <img src={menuImg} alt='menu'/>
+    <div className='Menu' onClick={handleEvent}>
+      <img src={menuImg} alt='menu' style={{cursor: 'pointer'}} />
+      <div className='Menu-options' id='menuOptions'>
+        <ul>
+          <li onClick={() => {
+            props.setSelectedBand('');
+            props.setShowCreateBand(false);
+            props.setBands(false);
+            props.setSearchError && props.setSearchError('');
+            props.setShowEditBand && props.setShowEditBand(false);
+            }}>Home</li>
+          <li onClick={() => {
+            props.setShowLogin(true);
+            }} className='Logout'>Logout</li>
+        </ul>
+      </div>
     </div>
   );
 }
