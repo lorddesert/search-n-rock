62487c8cc309e7c4f9dc9e496eb331055f95d0b4
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-18 01:12:26 -0300

    fixed 'go to home action' updating the state's of Home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,9 +8,24 @@ const Header = props => {
   return (
     <div className={`Header ${props.enableMenu && 'inHome'}`}>
       <main>
-        <h1>Search n' <strong className='highlighted'>Rock</strong></h1>
+        <h1 onClick={() => {
+          props.setSelectedBand(false);
+          props.setShowCreateBand(false);
+          props.setBands(false);
+          props.setSearchError && props.setSearchError('');
+          props.setShowEditBand && props.setShowEditBand(false);
+        }} >
+          Search n' <strong className='highlighted'>Rock</strong>
+        </h1>
         {props.enableMenu && 
-          <Menu />
+          <Menu
+            setSearchError={props.setSearchError}
+            setSelectedBand={props.setSelectedBand} 
+            setShowLogin={props.setShowLogin}
+            setShowCreateBand={props.setShowCreateBand}
+            setBands={props.setBands}
+            setShowEditBand={props.setShowEditBand}
+          />
         }
       </main>
     </div>
