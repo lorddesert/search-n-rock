9357fff2187281e5a49d55842c217b913b5fb72c
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-18 01:10:58 -0300

    band syntax changed and fixed, added some styles

--- src/components/Band/Band.jsx
+++ src/components/Band/Band.jsx
@@ -4,17 +4,31 @@ import './Band.css';
 
 const Band = props => {
   // Fetch the albums of the band, show the loader and then render.
-  return (
-    <div className='Band' onClick={() => props.fetchBand(props.key)}>
-      <main>
-        <hgroup>
-          <h1 className='Band-name'>{props.name}</h1>
-          <h2>{props.country}</h2>
-        </hgroup>
-        <div className='genre'>{props.genre}</div>
-      </main>
-    </div>
-  );
+  if(props.inBandDetails)
+    return (
+      <div className='Band inBandDetails'>
+        <main >
+          {/* <hgroup> */}
+            <h1 className='Band-name'>{props.band.name}</h1>
+            <h2>{props.band.country}</h2>
+            <h2>{props.band.year}</h2>
+          {/* </hgroup> */}
+          <div className='genre'>{props.band.genreCode}</div>
+        </main>
+      </div>
+    );
+  else
+    return (
+      <div className='Band' onClick={() => props.setSelectedBand(props.band.name)}>
+        <main>
+          <hgroup>
+            <h1 className='Band-name'>{props.band.name}</h1>
+            <h2>{props.band.country}</h2>
+          </hgroup>
+          <div className='genre'>{props.band.genreCode}</div>
+        </main>
+      </div>
+    );
 }
 
 export default Band;
