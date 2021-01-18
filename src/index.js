7e16cedd6bcf95679a8067563d617aa9a0427beb
Sacha Nocetto <sacha.nocetto@hotmail.com>
2021-01-18 00:54:01 -0300

    imported App component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App'
+import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+var abc = 'here!';
